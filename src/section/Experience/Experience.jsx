8cd2e8c729c0158c92904cea0d7d6e49e496c55b
SanthoshKumar211299 <subjansan@gmail.com>
2024-09-04 15:49:00 +0530

    experience added

---
+++ src/section/Experience/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Experience.module.css'
+const Experience = () => {
+  return (
+    <section id="experience" className={styles.container}>
+        <h1>Experience</h1>
+      <p>
+      this is my experience
+      </p>
+    </section>
+  )
+}
+
+export default Experience
