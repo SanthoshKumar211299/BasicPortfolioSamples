d7119a1fd5fa67c89972ea723dfd01b307137c2a
SanthoshKumar211299 <subjansan@gmail.com>
2024-09-16 12:33:27 +0530

    completed

---
+++ src/common/ExperienceCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ExperienceCard = ({h2,h3,p,h4}) => {
+  return (
+    <>
+    <br/>
+   <h2>{h2}</h2>
+   <h3>{h3}</h3>
+   <h3>{h4}</h3>
+   <p>{p}</p>
+   </>
+  )
+}
+
+export default ExperienceCard
--- src/section/Experience/Experience.jsx
+++ src/section/Experience/Experience.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import styles from './Experience.module.css'
+import ExperienceCard from '../../common/ExperienceCard'
 const Experience = () => {
   return (
     <section id="experience" className={styles.container}>
         <h1>Experience</h1>
-      <p>
-      this is my experience
-      </p>
+        <div>
+      <ExperienceCard  
+      h2= 'Software Developer & QA Support'
+      h3='CHUBB || Excess Casualty'
+      h4={'Jan 2022 to Apr 2023'}
+      p='Designed and implemented a casualty insurance application using Duck Creek
+Technologies, enhancing policy management . Leveraged Duck Creek’s suite of tools to
+streamline workflows and improve data accuracy. Increased operational efficiency by
+25% through custom automation and integration' />
+</div>
+<div>
+<ExperienceCard  
+      h2= 'Software Developer'
+      h3='CHUBB || Large Property Transformation'
+      h4={'Apr 2023 to Aug 2023'}
+      p='Designed and implemented a casualty insurance application using Duck Creek
+Technologies, enhancing policy management . Leveraged Duck Creek’s suite of tools to
+streamline workflows and improve data accuracy. Increased operational efficiency by
+25% through custom automation and integration' />
+</div>
     </section>
   )
 }
